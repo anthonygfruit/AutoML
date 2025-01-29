6731c52dbb54efde6c638fa6eb05d8e8e3efd2b9
AFruit <anthony.fruit@gmail.com>
2025-01-28 23:15:21 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TonyML',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/anthonygfruit/AutoML',
+    license='Apache 2.0',
+    author='AFruit',
+    author_email='anthony.fruit@gmail.com',
+    description='Testing out some automl shizzle'
+)
